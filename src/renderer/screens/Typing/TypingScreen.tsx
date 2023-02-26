0df0eced9b40a17175a8dc995ac35e053d9332eb
Ryan Lee <askryanlee@gmail.com>
2023-02-26 12:44:20 -0800

    Add layout and SideNav

--- src/renderer/screens/Typing/TypingScreen.tsx
+++ src/renderer/screens/Typing/TypingScreen.tsx
@@ -1,10 +1,22 @@
 import TypingBoard from 'renderer/components/TypingBoard/TypingBoard';
 import Screen from 'renderer/components/ui/Screen';
+import FileNav from 'renderer/components/SideNav/SideNav';
+import Row from 'renderer/components/ui/Row';
+import Column from 'renderer/components/ui/Column';
+
+const FILE_NAV_WIDTH = 350;
 
 export default function TypingScreen() {
   return (
     <Screen>
-      <TypingBoard />
+      <Row>
+        <Column width={`${FILE_NAV_WIDTH}px`}>
+          <FileNav />
+        </Column>
+        <Column>
+          <TypingBoard />
+        </Column>
+      </Row>
     </Screen>
   );
 }
