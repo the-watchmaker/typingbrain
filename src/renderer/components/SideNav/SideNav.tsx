081b8ccdd0785b07230db5984c9ab1233a80ed30
Ryan Lee <askryanlee@gmail.com>
2023-02-26 12:44:05 -0800

    create SideNav

---
+++ src/renderer/components/SideNav/SideNav.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const FileNavWrapper = styled.div`
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+export default function FileNav() {
+  return (
+    <FileNavWrapper>
+      <div>FileNav</div>;
+    </FileNavWrapper>
+  );
+}
