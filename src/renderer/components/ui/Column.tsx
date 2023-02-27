f090c50dc7793eeb2cf809b5a7096a5a5b8b2961
Ryan Lee <askryanlee@gmail.com>
2023-02-26 19:55:46 -0800

    use Column and Row to arrange layer instead of CSS

--- src/renderer/components/ui/Column.tsx
+++ src/renderer/components/ui/Column.tsx
@@ -4,7 +4,7 @@ const Column = styled.div<{ width?: string }>`
   display: flex;
   width: ${(props) => props.width || '100%'};
   height: 100%;
-  flex-direction: row;
+  flex-direction: column;
 `;
 
 export default Column;
--- src/renderer/screens/Typing/TypingScreen.tsx
+++ src/renderer/screens/Typing/TypingScreen.tsx
@@ -3,20 +3,26 @@ import Screen from 'renderer/components/ui/Screen';
 import FileNav from 'renderer/components/SideNav/SideNav';
 import Row from 'renderer/components/ui/Row';
 import Column from 'renderer/components/ui/Column';
+import TypingFooter from 'renderer/components/TypingFooter/TypingFooter';
 
 const FILE_NAV_WIDTH = 350;
 
 export default function TypingScreen() {
   return (
     <Screen>
-      <Row>
-        <Column width={`${FILE_NAV_WIDTH}px`}>
-          <FileNav />
-        </Column>
-        <Column>
-          <TypingBoard />
-        </Column>
-      </Row>
+      <Column>
+        <Row>
+          <Column width={`${FILE_NAV_WIDTH}px`}>
+            <FileNav />
+          </Column>
+          <Column>
+            <TypingBoard />
+          </Column>
+        </Row>
+        <Row height="32px">
+          <TypingFooter />
+        </Row>
+      </Column>
     </Screen>
   );
 }
