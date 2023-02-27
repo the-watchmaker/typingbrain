730b6440e581da7a050e7a036ebdc3c1028416c8
Ryan Lee <askryanlee@gmail.com>
2023-02-27 01:35:59 -0800

    Add FileNav

---
+++ src/renderer/components/SideNav/FileNav.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import SideNav from 'renderer/components/SideNav/SideNav';
+
+export default function FileNav() {
+  return (
+    <SideNav>
+      <div>FileNav</div>
+    </SideNav>
+  );
+}
